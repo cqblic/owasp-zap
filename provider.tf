ee5b3b96347a211d31119d9df9a47bde65e7150e
CQBLIC <carlos@blic.llc>
2026-05-07 13:18:59 -0700

    Fix: Remove CLI-only flag from provider and allow env-based auth

--- provider.tf
+++ provider.tf
@@ -1,10 +1,4 @@
 provider "azurerm" {
-  features {
-  }
-  subscription_id                 = "531799f9-99d5-4a8a-b8ad-6bb951d2fc7d"
-  environment                     = "public"
-  use_msi                         = false
-  use_cli                         = true
-  use_oidc                        = false
+  features {}
   resource_provider_registrations = "none"
 }
